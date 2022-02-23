b12570df07de05b0be192fe1e1f2c24b1809875d
mojie <jeason8023@gmail.com>
2022-02-23 16:12:56 +0800

    feat: 初始化项目

---
+++ packages/core/cli/README.md
@@ -0,0 +1,11 @@
+# @sun-fe/cli
+
+## 功能
+* 优先使用本地命令(import-local)
+
+## 流程
+
+
+## 第三方库
+* import-local
+* update-notifier
---
+++ packages/core/cli/bin/cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const importLocal = require("import-local");
+const log = require('@sun-fe/log');
+
+
+log.error("error");
+log.success("success");
+log.notice("notice");
+
+log.warn("warn");
+log.info("info");
+
+log.debug("debug");
+log.verbose("verbose");
